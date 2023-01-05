5f2dead13847e60b467e63bf120d994bd4b3788b
thenotoriousgustav <guscae4@gmail.com>
2023-01-06 03:04:57 +0700

    add meta tag for seo

--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,8 @@ export default function Head() {
   return (
     <>
       <title>GSTM STUDIO</title>
-      <meta content='width=device-width, initial-scale=1' />
-      <meta />
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta charSet='utf-8' />
       <link rel='icon' href='/favicon.ico' />
     </>
   );
